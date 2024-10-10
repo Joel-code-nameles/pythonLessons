e19894925b4c36586d1fc919c89701505a56e1e0
Joel-codes-nameles <anunyam.asare@gmail.com>
2024-10-10 16:49:58 -0700

    python update

---
+++ myshoppinglist = [].py
@@ -0,0 +1,15 @@
+myshoppinglist = []
+
+item1 = input("items: ")
+item2 = input("items: ")
+item3 = input("items: ")
+
+myshoppinglist.append(item1)
+myshoppinglist.append(item2)
+myshoppinglist.append(item3)
+
+if(myshoppinglist ):
+    print(myshoppinglist)
+
+else:
+    pass    
