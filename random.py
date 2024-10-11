a9bd18f557228d13e04fdc5af96eb01ccaf0081f
Joel-codes-nameles <anunyam.asare@gmail.com>
2024-10-10 17:49:21 -0700

    python update

--- random.py
+++ random.py
@@ -1,1 +1,19 @@
-print("hello world")
+
+boy_name = []
+girl_name = []
+
+name1 = input("name: ")
+name2 = input("name: ")
+name3 = input("name: ")
+name4 = input("name: ")
+
+
+boy_name.append(name1)
+boy_name.append(name2)
+
+print(boy_name)
+
+girl_name.append(name3)
+girl_name.append(name4)
+
+print(girl_name)
